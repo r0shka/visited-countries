80ae03ce6d2d383f87770c3ce6ddecc2ea984131
Vadym <roshkav2@gmail.com>
2024-02-02 22:59:23 +0100

    Setup sqldelight; Add countries data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("app.cash.sqldelight") version "2.0.1"
+}
+
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("com.r0shka.visitedcountries")
+        }
+    }
 }
 
 android {
@@ -56,6 +65,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation("app.cash.sqldelight:android-driver:2.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
