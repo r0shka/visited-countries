29a4e8935ed5eedf483e44289ddeaa7e9ec9756c
Vadym <roshkav2@gmail.com>
2024-02-16 01:07:38 +0100

    Add db to domain entity mappers

--- app/src/main/java/com/r0shka/visitedcountries/domain/entities/Country.kt
+++ app/src/main/java/com/r0shka/visitedcountries/domain/entities/Country.kt
@@ -1,5 +1,7 @@
 package com.r0shka.visitedcountries.domain.entities
 
+import com.r0shka.visitedcountries.DbCountry
+
 data class Country(
     val codeCca3: String,
     val codeCca2: String,
@@ -9,45 +11,81 @@ data class Country(
     val drivingSide: DrivingSide,
     val continent: Continent,
     val region: Region
+) {
+    fun toDatabase(): DbCountry = DbCountry(
+        country_code_cca3 = codeCca3,
+        country_code_cca2 = codeCca2,
+        is_independent = if (isIndependent) 1 else 0,
+        is_un_member = if (isUnMember) 1 else 0,
+        population = population,
+        driving_side = drivingSide.value,
+        continent = continent.value,
+        region = region.value,
+        flag_svg_url = "",
     )
-
-enum class DrivingSide {
-    LEFT,
-    RIGHT,
 }
 
-enum class Continent {
-    EUROPE,
-    ASIA,
-    NORTH_AMERICA,
-    SOUTH_AMERICA,
-    AFRICA,
-    OCEANIA,
-    ANTARCTICA,
+fun DbCountry.toDomain(): Country = Country(
+    codeCca3 = country_code_cca3,
+    codeCca2 = country_code_cca2,
+    isIndependent = is_independent == 1L,
+    isUnMember = is_un_member == 1L,
+    population = population,
+    drivingSide = DrivingSide.from(driving_side),
+    continent = Continent.from(continent),
+    region = Region.from(region),
+)
+
+enum class DrivingSide(val value: String) {
+    LEFT("left"),
+    RIGHT("right");
+
+    companion object {
+        infix fun from(value: String): DrivingSide = entries.first { it.value == value }
+    }
 }
 
-enum class Region {
-    SOUTHERN_EUROPE,
-    NORTHERN_EUROPE,
-    WESTERN_EUROPE,
-    SOUTHEAST_EUROPE,
-    EASTERN_EUROPE,
-    CENTRAL_EUROPE,
-    NORTH_AMERICA,
-    CENTRAL_AMERICA,
-    CARIBBEAN,
-    SOUTH_AMERICA,
-    WESTERN_AFRICA,
-    EASTERN_AFRICA,
-    MIDDLE_AFRICA,
-    SOUTHERN_AFRICA,
-    SOUTH_EASTERN_ASIA,
-    WESTERN_ASIA,
-    SOUTHERN_ASIA,
-    EASTERN_ASIA,
-    CENTRAL_ASIA,
-    MELANESIA,
-    POLYNESIA,
-    MICRONESIA,
-    AUSTRALIA_AND_NEW_ZEALAND
+enum class Continent(val value: String) {
+    EUROPE("Europe"),
+    ASIA("Asia"),
+    NORTH_AMERICA("North America"),
+    SOUTH_AMERICA("South America"),
+    AFRICA("Africa"),
+    OCEANIA("Oceania"),
+    ANTARCTICA("Antarctica");
+
+    companion object {
+        infix fun from(value: String): Continent = entries.first { it.value == value }
+    }
+}
+
+enum class Region(val value: String) {
+    SOUTHERN_EUROPE("Southern Europe"),
+    NORTHERN_EUROPE("Northern Europe"),
+    WESTERN_EUROPE("Western Europe"),
+    SOUTHEAST_EUROPE("Southeast Europe"),
+    EASTERN_EUROPE("Eastern Europe"),
+    CENTRAL_EUROPE("Central Europe"),
+    NORTH_AMERICA("North America"),
+    CENTRAL_AMERICA("Central America"),
+    CARIBBEAN("Caribbean"),
+    SOUTH_AMERICA("South America"),
+    WESTERN_AFRICA("Western Africa"),
+    EASTERN_AFRICA("Eastern Africa"),
+    MIDDLE_AFRICA("Middle Africa"),
+    SOUTHERN_AFRICA("Southern Africa"),
+    NORTHERN_AFRICA("Northern Africa"),
+    SOUTH_EASTERN_ASIA("South-Eastern Asia"),
+    WESTERN_ASIA("Western Asia"),
+    SOUTHERN_ASIA("Southern Asia"),
+    EASTERN_ASIA("Eastern Asia"),
+    CENTRAL_ASIA("Central Asia"),
+    MELANESIA("Melanesia"),
+    POLYNESIA("Polynesia"),
+    MICRONESIA("Micronesia"),
+    AUSTRALIA_AND_NEW_ZEALAND("Australia and New Zealand");
+
+    companion object {
+        infix fun from(value: String): Region = entries.first { it.value == value }
+    }
 }
--- app/src/main/java/com/r0shka/visitedcountries/domain/entities/Visit.kt
+++ app/src/main/java/com/r0shka/visitedcountries/domain/entities/Visit.kt
@@ -1,9 +1,21 @@
 package com.r0shka.visitedcountries.domain.entities
 
+import com.r0shka.visitedcountries.DbVisit
 import java.util.UUID
 
 data class Visit(
     val id: String = UUID.randomUUID().toString(),
     val countryCodeCca3: String,
     val visitedAt: Long,
+) {
+    fun toDatabase(): DbVisit = DbVisit(
+        id = id,
+        country_code_cca3 = countryCodeCca3,
+        date = visitedAt,
+    )
+}
+
+fun DbVisit.toDomain(): Visit = Visit(
+    countryCodeCca3 = country_code_cca3,
+    visitedAt = date,
 )
